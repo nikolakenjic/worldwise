c33a68d9142fd006073ec3ca35ec33026d919aaa
Nikola Kenjic <nikola.nico89@gmail.com>
2024-07-03 19:57:31 +0200

    worldwise

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Login.module.css';
 import { PageNav } from '../components';
+import { useAuth } from '../context/FakeAuthContext';
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const { login, isAuthenticated } = useAuth();
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com');
   const [password, setPassword] = useState('qwerty');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated === true) navigate('/app', { replace: true });
+  }, [isAuthenticated, navigate]);
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +47,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/FakeAuthContext';
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate('/');
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? children : null;
+};
+
+export default ProtectedRoute;
